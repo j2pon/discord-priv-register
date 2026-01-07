aec4a57960cd63570057b66d06a1633ddc6f1136
j2pon <54667804+j2pon@users.noreply.github.com>
2026-01-08 01:31:30 +0300

    Update vip.js

--- commands/vip.js
+++ commands/vip.js
@@ -1,29 +1,29 @@
-const { rexstaff, vip, emojitik } = require('../config.json');
+const { j2ponstaff, vip, emojitik } = require('../config.json');
 
 module.exports = {
     name: 'vip',
     async execute(message, args) {
-        if (!message.member.roles.cache.has(rexstaff) && !message.member.permissions.has('ADMINISTRATOR')) {
+        if (!message.member.roles.cache.has(j2ponstaff) && !message.member.permissions.has('ADMINISTRATOR')) {
             return message.channel.send('Bu komutu kullanmak için gerekli yetkin yok.');
         }
 
-        const rex = args[0]?.replace(/[<@!>]/g, '');
-        if (!rex) {
+        const j2pon = args[0]?.replace(/[<@!>]/g, '');
+        if (!j2pon) {
             return message.channel.send('Lütfen Vip vermek istediğiniz kişiyi etiketleyin ya da kullanıcı id\'sini giriniz.');
         }
 
-        const member = await message.guild.members.fetch(rex).catch(() => null);
+        const member = await message.guild.members.fetch(j2pon).catch(() => null);
         if (!member) {
             return message.channel.send('Belirtilen kullanıcı bulunamadı.');
         }
 
-        const rexvip = message.guild.roles.cache.get(vip);
-        if (!rexvip) {
+        const j2ponvip = message.guild.roles.cache.get(vip);
+        if (!j2ponvip) {
             return message.channel.send('Rol bulunamadı.');
         }
 
         try {
-            await member.roles.add([rexvip]);
+            await member.roles.add([j2ponvip]);
             await message.react(emojitik);
         } catch (error) {
             console.error('Rol eklenirken bir hata oluştu:', error);
@@ -31,3 +31,4 @@ module.exports = {
         }
     },
 };
+
